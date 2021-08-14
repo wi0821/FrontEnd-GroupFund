314ae72c847fe352ddddd6fa369729a6cd92cc4b
WilsonHan <Win05118@gmail.com>
2021-08-15 05:51:00 +0800

    Update 2021-08-14T21:50:57.917Z

---
+++ assets/js/all.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var swiper = new Swiper(".mySwiper", {
+  autoplay: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true
+  },
+  breakpoints: {
+    768: {
+      autoplay: true
+    }
+  }
+}); // var curPage = $(document).attr('title');
+// $(document).ready(function() {
+//   $( ".header-List" ).click(function() {
+//     if (curPage ==)
+//     this.toggleClass("disabled");
+//   });
+// });
+// $(function(){
+//   $( "#buy" ).click(function() {
+//     $("#bbb").toggleClass("d-none");
+//   });
+// });
+//# sourceMappingURL=all.js.map
